ef728f65c5a6e57c84b6f784bd8858fcc9285c4a
Tanishk Raghav <tanishkraghav2004@gmail.com>
2026-03-25 16:35:21 +0530

    ..

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE || 'https://reconciler-backend-dprj.onrender.com',
+  baseURL: import.meta.env.VITE_API_BASE || 'https://reconciler-backend-h8dk.onrender.com',
 });
 
 api.interceptors.request.use((config) => {
